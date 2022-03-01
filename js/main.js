f5a70afc5571d737d37c0ea44a95b0d4658e56f0
Lucas Brandão <lucasbrandao392@gmail.com>
2022-03-01 19:20:20 -0300

    JavaScript

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var typed3 = new Typed('#mudar', {
+    strings: ['Designer', 'Web Developer', 'Freelancer', 'Computer Technician'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    smartBackspace: true, // this is a default
+    loop: true
+});
